10e4a269800be20d5175fb9901ae82e3ae3e82de
PaintX <remy.arnaudin@wanadoo.fr>
2016-01-14 17:37:45 +0100

    - classeur de calcul

--- Frequencemetre/Frequencemetre.ino
+++ Frequencemetre/Frequencemetre.ino
@@ -1,4 +1,7 @@
 #include <LiquidCrystal.h>
+#include "avance_tab.h"
+
+
 
 LiquidCrystal lcd(8,9,4,5,6,7);
 int adc_key_in;
@@ -6,6 +9,7 @@ volatile int state = LOW;
 
 void setup() 
 {
+  S_AVANCE * ptr;
   pinMode(A0, INPUT);
   Serial.begin(115200);
   lcd.begin(16,2);
@@ -13,16 +17,35 @@ void setup()
   Serial.println("Hello World");
   analogReference(EXTERNAL);
   attachInterrupt(digitalPinToInterrupt(3), trigPIN3, RISING); 
+
+
+  ptr = AVANCE_TAB_GetTab();
+  for ( int i = 0 ; i < AVANCE_TAB_GetSize() ; i++ )
+  {
+    ptr[i].avance = (float)i;
+  }
+
+  Serial.println("**************************");
+  Serial.println("table d'avance :");
+    for ( int i = 0 ; i < AVANCE_TAB_GetSize() ; i++ )
+  {
+    Serial.println(ptr[i].avance);
+  }
 }
 
-static unsigned long  delayMs;
+static unsigned long  periode;
 static unsigned long ms;
 float freq = 0.0;
 float trmin = 0.0;
+
+
+
 void loop() 
 {
+  
+  
   lcd.setCursor(0,0);            // move cursor to second line "1" and 9 spaces over
-  freq = 1.0/((float)delayMs/1000000.0);
+  freq = 1.0/((float)periode/1000000.0);
   lcd.print(freq);
   lcd.print(" Hz");
 
@@ -40,6 +63,6 @@ unsigned long us;
 void trigPIN3() 
 {
   us = micros();
-  delayMs = us - ms;
+  periode = us - ms;
   ms = us;
 }
--- Frequencemetre/avance_tab.h
+++ Frequencemetre/avance_tab.h
@@ -7,8 +7,7 @@ typedef struct
   float   avance;
 }S_AVANCE;
 
-
-
 unsigned int AVANCE_TAB_GetSize(void);
 S_AVANCE * AVANCE_TAB_GetTab(void);
+
 #endif
